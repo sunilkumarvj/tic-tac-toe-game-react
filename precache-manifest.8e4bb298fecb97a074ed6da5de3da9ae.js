1199d5e00bf8ac56a645155986b3cca029640c51
V J <sunilkumara.v.j@accenture.com>
2020-02-20 10:55:38 +0530

    Updates

---
+++ precache-manifest.8e4bb298fecb97a074ed6da5de3da9ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb5a9aa109f4a4dc07d1bc3ca5cd2af",
+    "url": "/tic-tac-toe-game-react/index.html"
+  },
+  {
+    "revision": "77c74e080d2e43c8a51c",
+    "url": "/tic-tac-toe-game-react/static/css/main.898c4e57.chunk.css"
+  },
+  {
+    "revision": "9ada8a60b5731fcba4c1",
+    "url": "/tic-tac-toe-game-react/static/js/2.053cf5bb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe-game-react/static/js/2.053cf5bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77c74e080d2e43c8a51c",
+    "url": "/tic-tac-toe-game-react/static/js/main.37289db8.chunk.js"
+  },
+  {
+    "revision": "b4b5bcf5a0e8bfca5c19",
+    "url": "/tic-tac-toe-game-react/static/js/runtime-main.62130ca4.js"
+  },
+  {
+    "revision": "a385155b3fa32c706a7a4f76d898d9cc",
+    "url": "/tic-tac-toe-game-react/static/media/s3.a385155b.jfif"
+  }
+]);
